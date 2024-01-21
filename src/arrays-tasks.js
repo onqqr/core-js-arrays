f95ffb6a2312d7e77b1633e866178f77d5ad6b1b
onqqr <dnbyaniss@gmail.com>
2024-01-21 10:04:38 +0300

    feat: solved shiftArray

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -539,8 +539,11 @@ function propagateItemsByPositionIndex(/* arr */) {
  *    shiftArray(['a', 'b', 'c', 'd'], -1) => ['b', 'c', 'd', 'a']
  *    shiftArray([10, 20, 30, 40, 50], -3) => [40, 50, 10, 20, 30]
  */
-function shiftArray(/* arr, n */) {
-  throw new Error('Not implemented');
+function shiftArray(arr, n) {
+  const arrSlice = arr.slice();
+  const shift = ((n % arr.length) + arr.length) % arr.length;
+
+  return arrSlice.splice(-shift).concat(arrSlice);
 }
 
 /**
